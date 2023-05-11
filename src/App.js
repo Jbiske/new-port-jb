e26b50444fdc31188433820a1475bd0e329d7306
Josh <biscuitemail2@gmail.com>
2023-05-10 23:15:14 -0400

    yes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About   from "./components/About";
 import  Projects from './components/Projects'
 import  Home  from './components/Home';
 import Contact from "./components/Contact";
-import Aos from 'aos'
+
 import 'aos/dist/aos.css';
 
 
